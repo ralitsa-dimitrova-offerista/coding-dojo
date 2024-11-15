8dbddfc0fb8bc85fe89622add3b7eab14723c20a
Christian <christian.winkel@offerista.com>
2024-11-15 10:33:40 +0100

    save

--- tabulate-csv/app/src/main/java/fizzbuzz/FizzBuzz.java
+++ tabulate-csv/app/src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 public class FizzBuzz {
     public static void main(String[] args) throws IOException {            
         for (int i = 1; i <= 100; i++) {
+            String iStr = String.valueOf(i);
+
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (i % 3 == 0 || iStr.contains("3")) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (i % 5 == 0 || iStr.contains("5")) {
                 System.out.println("Buzz");  
             } else {
                 System.out.println(i);
